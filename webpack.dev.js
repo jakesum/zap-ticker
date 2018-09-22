9f39c45607923b60e05a35736b951e1bbfbaeaa6
losh11 <loshan1212@gmail.com>
2018-09-22 02:04:14 +0100

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [{
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader',
+
+      options: {
+        plugins: ['syntax-dynamic-import'],
+
+        presets: [['env', {
+          'modules': false
+        }]]
+      },
+
+      test: /\.js$/
+    }]
+  },
+
+  entry: './app',
+
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: 'development',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
+}
