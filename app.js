6d126012cc243585707b09d869a46206536b783d
losh11 <loshan1212@gmail.com>
2018-09-22 06:49:04 +0100

    Add ticker

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
+import axios from 'axios'
+import cors from 'cors'
 import express from 'express'
+import fiat from './fiat'
+import helmet from 'helmet'
 
 const app = express()
-
 const PORT = process.env.PORT || 3004
 
-app.get('/', (req, res) => {
-    res.send('hello')
+let bitcoinRates = {}
+let litecoinRates = {}
+
+app.use(cors())
+app.use(helmet())
+
+// rate fetching function
+const fetchCrypto = (currency) => {
+    return axios.get(`https://api.coinbase.com/v2/exchange-rates?currency=${currency}`)
+}
+
+// fetch Bitcoin & Litecoin rates from Coinbase API
+setInterval(() => {
+    axios.all([fetchCrypto('BTC'), fetchCrypto('LTC')])
+        .then(axios.spread((bitcoin, litecoin) => {
+
+            bitcoin = bitcoin.data.data.rates
+            litecoin = litecoin.data.data.rates
+
+            Object.keys(bitcoin).map((key) => {
+                bitcoinRates[key] = { price: bitcoin[key], symbol: fiat[key].symbol }
+            })
+
+            Object.keys(litecoin).map((key) => {
+                litecoinRates[key] = { price: litecoin[key], symbol: fiat[key].symbol }
+            })
+        }))
+}, 2000)
+
+app.get('/ticker/bitcoin', (req, res) => {
+    res.json(bitcoinRates)
+})
+
+app.get('/ticker/litecoin', (req, res) => {
+    res.json(litecoinRates)
 })
 
 app.listen(PORT, () => {
-    console.log(`ltc-ticker is running on localhost:${PORT}`)
+    console.log(`zap-ticker is running on localhost:${PORT}`)
 })
--- webpack.js
+++ webpack.js
@@ -34,7 +34,7 @@ module.exports = {
     }]
   },
 
-  mode: 'development',
+  mode: process.env.NODE_ENV,
 
   optimization: {
     splitChunks: {
